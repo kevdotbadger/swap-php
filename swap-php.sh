10047a350473f92eaef740faf65c4da279fad4a1
Kevin Ruscoe <kevin@popcreative.co.uk>
2019-02-09 12:06:46 +0000

    clean up

--- swap-php.sh
+++ swap-php.sh
@@ -2,16 +2,24 @@
 
 read -p "Which version? [7.1, 7.2, 7.3] " version
 
-sudo brew services stop php@7.1
-sudo brew services stop php@7.2
-sudo brew services stop php@7.3
+echo "Stopping php-fpm services"
+sudo brew services stop php@7.1 >> /dev/null 2>&1
+sudo brew services stop php@7.2 >> /dev/null 2>&1
+sudo brew services stop php@7.3 >> /dev/null 2>&1
 
-brew unlink php
-brew unlink php@7.1
-brew unlink php@7.2
-brew unlink php@7.3
+echo "Unlinking php"
+brew unlink php >> /dev/null 2>&1
+brew unlink php@7.1 >> /dev/null 2>&1
+brew unlink php@7.2 >> /dev/null 2>&1
+brew unlink php@7.3 >> /dev/null 2>&1
 
-brew link php@$version --force
+echo "Relinking php@$version"
+brew link php@$version --force >> /dev/null 2>&1
 
-valet uninstall
-valet install
+echo "Refreshing Valets installation"
+valet uninstall >> /dev/null 2>&1
+valet install >> /dev/null 2>&1
+
+echo "Done"
+which php
+php -v
